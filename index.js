5d739faa8b73372d0f7415a644dba1c53d889127
renae carter <rkwalker89@gmail.com>
2022-07-11 13:30:26 -0400

    commit tree 2e890a33844f322914216fa61748f972f6fc6504

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+undefined
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+undefined
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+  
+    if ( left < 360) {
+      dodger.style.left = `${left + 4}px`;
+    }
+  }
+        
+  document.addEventListener('keydown', function(e) {
+    if ( e.which === 37 ) {
+      moveDodgerLeft();
+    }
+    if ( e.which === 39 ) {
+      moveDodgerRight();
+    }
+  });
